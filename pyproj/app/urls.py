4d9b8224e3f2a65995ba98dc8dd9fbb08777aa7b
Mitch Davis <mjd@afork.com>
2025-10-26 03:15:57 +1100

    Added first cut of code to parse a string as a possible barcode.

--- pyproj/app/urls.py
+++ pyproj/app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('partials/total-items/', views.total_items_partial, name='total_items_partial'),
     path('item/', views.item_list, name='item_list'),
     path('item/<int:pk>/', views.item_detail, name='item_detail'),
+    path('item/new/', views.new_item, name='new_item'),
+    path('scan/', views.scan_redirect, name='scan_redirect'),
 ]
--- pyproj/app/views.py
+++ pyproj/app/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Item
+import re
+from urllib.parse import urlencode
+
+from django.conf import settings
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+
+
+from .models import ExternalBarcode, Item
 
 
 def inventory_dashboard(request):
@@ -14,6 +22,43 @@ def total_items_partial(request):
     total_items = Item.objects.filter(deleted=False).count()
     context = {'total_items': total_items}
     return render(request, 'app/dash.html#total-items-card', context)
+
+
+def scan_redirect(request):
+    """Handle barcode scanning - redirect to item or new item page"""
+    code = request.GET.get('barcode', '').strip()
+
+    # If q is empty or missing, return 404
+    if not code:
+        raise Http404("No barcode provided")
+
+    # Try to find item by internal barcode using regex
+    match = re.match(f"^{re.escape(settings.BARCODE_PREFIX)}(\\d+)$", code)
+    if match:
+        try:
+            item = Item.objects.get(id=match.group(1))
+            # return redirect('app:item_detail', pk=item.id)
+            return redirect(item)
+        except Item.DoesNotExist:
+            pass
+
+    # Try to find item by external barcode
+    try:
+        external_barcode = ExternalBarcode.objects.get(code=code)
+        return redirect(external_barcode.item)
+    except ExternalBarcode.DoesNotExist:
+        pass
+
+    # If no item found (could be new!), redirect to new item page
+    url = reverse('app:new_item', query={'barcode': code})
+    return HttpResponseRedirect(url)
+
+
+def new_item(request):
+    """Display a form for creating a new item"""
+    barcode = request.GET.get('barcode', '').strip()
+    context = {'barcode': barcode}
+    return render(request, 'app/new_item.html', context)
 
 
 def item_list(request):
