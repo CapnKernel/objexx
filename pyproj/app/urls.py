486ca3f9fc1784dbcef8ff9187ba9558cb3ea830
Mitch Davis <mjd@afork.com>
2025-10-26 02:11:45 +1100

    Change inventory_dashboard URL pattern name to dash.

--- pyproj/app/urls.py
+++ pyproj/app/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'app'
 
 urlpatterns = [
-    path('', views.inventory_dashboard, name='inventory_dashboard'),
+    path('', views.inventory_dashboard, name='dash'),
     path('partials/total-items/', views.total_items_partial, name='total_items_partial'),
     path('item/', views.item_list, name='item_list'),
     path('item/<int:pk>/', views.item_detail, name='item_detail'),
