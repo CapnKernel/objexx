8db980e9f77f209af1c3e49b600ab29e6324bc40
Mitch Davis <mjd@afork.com>
2025-10-26 00:27:22 +1100

    Added models to the admin

--- pyproj/app/admin.py
+++ pyproj/app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Item, ExternalBarcode, ItemHistory
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent', 'deleted', 'created_at']
+    list_filter = ['deleted', 'created_at']
+    search_fields = ['name', 'description']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(ExternalBarcode)
+class ExternalBarcodeAdmin(admin.ModelAdmin):
+    list_display = ['code', 'item', 'barcode_type', 'created_at']
+    list_filter = ['barcode_type', 'created_at']
+    search_fields = ['code', 'item__name']
+    readonly_fields = ['created_at']
+
+
+@admin.register(ItemHistory)
+class ItemHistoryAdmin(admin.ModelAdmin):
+    list_display = ['item', 'action', 'user', 'timestamp']
+    list_filter = ['action', 'timestamp']
+    search_fields = ['item__name', 'description']
+    readonly_fields = ['timestamp']
