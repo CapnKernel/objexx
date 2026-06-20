a73648aacdc75218258268cc1041707d808ad330
Mitch Davis <mjd+github@afork.com>
2026-06-20 18:37:38 +1000

    Fixed a crash on display of an item if last_updated_at wasn't set.

--- src/app/views.py
+++ src/app/views.py
@@ -244,7 +244,10 @@ def item_detail(request, pk):
     """Display details for a specific item"""
     item = get_object_or_404(Item, pk=pk)
     tree_structure = item.get_contained_tree() if item.is_container else None
-    updated_when_scanned = abs(item.last_updated_at - item.last_scanned_at) < timedelta(seconds=1)
+    updated_when_scanned = (
+        item.last_scanned_at is not None
+        and abs(item.last_updated_at - item.last_scanned_at) < timedelta(seconds=1)
+    )
     context = {
         'item': item,
         'tree_structure': tree_structure,
